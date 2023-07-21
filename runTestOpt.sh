be7ca4e36e49ce54693e9d97125d9d57d0e82a57
Ned Loynd <ned@loynd.com>
2023-07-21 18:28:23 +1000

    Bonus test

--- runTestOpt.sh
+++ runTestOpt.sh
@@ -3,3 +3,4 @@ gradle build
 java -ea -jar ./build/libs/deft4j-all.jar ./test/asyoulik/asyoulik-gzip.txt.gz ./test/asyoulik/asyoulik-gzip-opt.txt.gz
 java -ea -jar ./build/libs/deft4j-all.jar ./test/asyoulik/asyoulik-zopfli.txt.gz ./test/asyoulik/asyoulik-zopfli-opt.txt.gz
 java -ea -jar ./build/libs/deft4j-all.jar ./test/nerd/nerd.png ./test/nerd/nerd-opt.png
+java -ea -jar ./build/libs/deft4j-all.jar ./test/nerd/nerd-extopt.png ./test/nerd/nerd-fullopt.png
