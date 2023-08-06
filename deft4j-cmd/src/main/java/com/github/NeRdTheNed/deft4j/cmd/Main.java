5b298120902f3b852807dddd4a4d821cc758334b
Ned Loynd <ned@loynd.com>
2023-08-06 16:54:19 +1000

    Change version scheme

--- deft4j-cmd/src/main/java/com/github/NeRdTheNed/deft4j/cmd/Main.java
+++ deft4j-cmd/src/main/java/com/github/NeRdTheNed/deft4j/cmd/Main.java
@@ -3,7 +3,7 @@ package com.github.NeRdTheNed.deft4j.cmd;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
-@Command(name = "deft4j", version = "deft4j v1.0.0", subcommands = { Optimise.class, ConvertZip.class })
+@Command(name = "deft4j", version = "deft4j v1.0.0-beta", subcommands = { Optimise.class, ConvertZip.class })
 public class Main {
     public static void main(String[] args) {
         final int exitCode = new CommandLine(new Main()).execute(args);
