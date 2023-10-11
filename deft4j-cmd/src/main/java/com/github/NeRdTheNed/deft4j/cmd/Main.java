cf5dd67a1507625849fd4597da5d53a99f7cf489
Ned Loynd <ned@loynd.com>
2023-10-11 11:12:47 +1100

    Bump version

--- deft4j-cmd/src/main/java/com/github/NeRdTheNed/deft4j/cmd/Main.java
+++ deft4j-cmd/src/main/java/com/github/NeRdTheNed/deft4j/cmd/Main.java
@@ -3,7 +3,7 @@ package com.github.NeRdTheNed.deft4j.cmd;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
-@Command(name = "deft4j", version = "deft4j v1.0.0-beta-16", subcommands = { Optimise.class, OptimiseFolder.class, ConvertToGZip.class, GZipCompress.class })
+@Command(name = "deft4j", version = "deft4j v1.0.0-beta-17", subcommands = { Optimise.class, OptimiseFolder.class, ConvertToGZip.class, GZipCompress.class })
 public class Main {
     public static void main(String[] args) {
         final int exitCode = new CommandLine(new Main()).execute(args);
